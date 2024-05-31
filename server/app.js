ff4f41e9f9d516b89462b114060a9b7f38c49cd3
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-05-31 14:55:46 +0300

    app content

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
-import React from 'react'
+const express = require('express')
+const http = require('http')
+const Server = require('socket.io').Server
 
-const app = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const app = express()
 
-export default app
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("connected");
+
+    socket.on("chat", chat => {
+        io.emit("chat", chat)
+    })
+
+    socket.on("disconnect", () => {
+        console.log("disconnect")
+    })
+})
+
+server.listen("3002", () => {
+    console.log("running on 3002 port")
+})
